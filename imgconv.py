0b23db4347ce1b1c2f6fb8974861e855537182c3
Marouane Rguibi <rg.marouane@gmail.com>
2023-10-11 10:09:50 +0100

    Update imgconv.py

--- imgconv.py
+++ imgconv.py
@@ -1,13 +1,24 @@
 import os
 from PIL import Image
 
-def convert_images_to_webp(input_folder, output_folder):
+# Define a global folder for storing old image files
+old_images_folder = "old_pic"
+
+def convert_images_to_webp(input_folder, output_folder=None):
+    # If output folder is not specified, set it to the same as the input folder
+    if output_folder is None:
+        output_folder = input_folder
+
     # Create the output folder if it doesn't exist
     if not os.path.exists(output_folder):
         os.makedirs(output_folder)
 
+    # Create the "old_pic" folder if it doesn't exist
+    if not os.path.exists(old_images_folder):
+        os.makedirs(old_images_folder)
+
     for filename in os.listdir(input_folder):
-        # Check if the file is an image (PNG/JPG)
+        # Check if the file is an image (PNG/JPG/JPEG)
         if filename.lower().endswith(('.png', '.jpg', '.jpeg')):
             input_path = os.path.join(input_folder, filename)
             output_path = os.path.join(output_folder, os.path.splitext(filename)[0] + '.webp')
@@ -25,14 +36,17 @@ def convert_images_to_webp(input_folder, output_folder):
 
             print(f"Converted {filename} to WebP. Size difference: {size_difference} bytes ({percentage_difference:.2f}%).")
 
-# Prompt the user for input and output folder paths
+            # Move the original image file to the "old_pic" folder
+            old_image_path = os.path.join(old_images_folder, filename)
+            os.rename(input_path, old_image_path)
+            print(f"Moved {filename} to 'old_pic' folder.")
+
+# Prompt the user for input folder
 input_folder = input("Enter the path to the input folder: ")
-output_folder = input("Enter the path to the output folder: ")
+# Prompt the user for output folder (or leave it blank to use the same as input folder)
+output_folder = input("Enter the path to the output folder (or leave it blank to use the same as input folder): ")
+
+if not output_folder.strip():
+    output_folder = None
 
 convert_images_to_webp(input_folder, output_folder)
-
-
-
-
-
-
